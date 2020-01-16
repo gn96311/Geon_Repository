709236de77c5e9a22e7db7db978895834910b736
gn96311 <su52540@naver.com>
2020-01-16 20:25:55 +0900

    2020_01_16-2

--- Baekjoon/Range/Coordinate_range.py
+++ Baekjoon/Range/Coordinate_range.py
@@ -1,3 +1,19 @@
+N = int(input())
+
+result = []
+for i in range(N):
+    X, Y = list(map(int, input().split()))
+    result.append([X,Y])
+
+result.sort(key = lambda x: (x[0], x[1]))
+for j in result:
+    print(j[0], j[1])
+
+#sys.stdin은 input보다 속도가 빠르다.
+
+#sort 함수에서 key를 설정하면, 특정 기준으로 정렬한다.
+
+'''
 import sys
 
 N = int(sys.stdin.readline())
@@ -7,3 +23,4 @@ for i in range(N):
 result.sort(key = lambda x: (x[0], x[1]))
 for j in result:
     print(j[0], j[1])
+'''
